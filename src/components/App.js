65054db5818fae277cfd3f5ffb39df78528cf448
andrewjensen <andrew.jensen-battaglia@jorsek.com>
2017-06-26 21:13:59 -0400

    hook up firebase to App, add remove book method

--- src/components/App.js
+++ src/components/App.js
@@ -4,77 +4,88 @@ import Logo from './Logo';
 import SearchBar from './SearchBar';
 import ListPicker from './ListPicker';
 import BookListPane from './BookListPane';
-
-// remove book
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
     super();
     
     this.state = {
-      bookLists: {
-        
-        'to-read': {
-          listName: 'To Read',
-          name: 'to-read',
-          selected: true,
-          books: {
-            'book-1': {
-              title: 'To Kill A Mockingbird',
-              author: 'Harper Lee',
-              pubDate: '1952',
-              img: 'url-to-image',
-              list: 'to-read'
-            },
-            'book-2:': {
-              title: 'The Tempest',
-              author: 'William Shakespeare',
-              pubDate: '1519',
-              img: 'url-to-image',
-              list: 'to-read'  
-            }
-          }
-        },
-        
-        'read': {
-          listName: 'Read',
-          name: 'read',
-          selected: false,
-          books: {
-            'book-3': {
-              title: 'Ragtime',
-              author: 'E.L. Doctorow',
-              pubDate: '1931',
-              img: 'url-to-image',
-              list: 'read'
-            },
-            'book-4:': {
-              title: 'Middlesex',
-              author: 'Jeffery Eugenides',
-              pubDate: '2004',
-              img: 'url-to-image',
-              list: 'read'  
-            }
-          }
-        },
-      
-        'favorites': {
-          listName: 'Favorites',
-          name: 'favorites',
-          selected: false,
-          books: {
-            'book-5': {
-              title: 'Infinite Jest',
-              author: 'David Foster Wallace',
-              pubDate: '1994',
-              img: 'url-to-image',
-              list: 'favorites'
-            }
-          }
-        }
-      
-      }
+      bookLists: {}
     };
+  }
+  
+  componentWillMount() {
+    this.ref = base.syncState('bookLists',
+      {
+        context: this,
+        state: 'bookLists',
+        defaultValue: {
+          'to-read': {
+            listName: 'To Read',
+            name: 'to-read',
+            selected: true,
+            books: {
+              'book-1': {
+                title: 'To Kill A Mockingbird',
+                author: 'Harper Lee',
+                pubDate: '1952',
+                img: 'url-to-image',
+                list: 'to-read'
+              },
+              'book-2:': {
+                title: 'The Tempest',
+                author: 'William Shakespeare',
+                pubDate: '1519',
+                img: 'url-to-image',
+                list: 'to-read'  
+              }
+            }
+          },
+          
+          'read': {
+            listName: 'Read',
+            name: 'read',
+            selected: false,
+            books: {
+              'book-3': {
+                title: 'Ragtime',
+                author: 'E.L. Doctorow',
+                pubDate: '1931',
+                img: 'url-to-image',
+                list: 'read'
+              },
+              'book-4:': {
+                title: 'Middlesex',
+                author: 'Jeffery Eugenides',
+                pubDate: '2004',
+                img: 'url-to-image',
+                list: 'read'  
+              }
+            }
+          },
+        
+          'favorites': {
+            listName: 'Favorites',
+            name: 'favorites',
+            selected: false,
+            books: {
+              'book-5': {
+                title: 'Infinite Jest',
+                author: 'David Foster Wallace',
+                pubDate: '1994',
+                img: 'url-to-image',
+                list: 'favorites'
+              }
+            }
+          }
+          
+        }
+      });
+  }
+  
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
   
   // newBook will come from api
@@ -85,6 +96,16 @@ class App extends React.Component {
     books[`book-${id}`] = newBook;
     this.setState({
       bookLists: update(this.state.bookLists, {[listName]: {books: {$set: books}}})
+    });
+  };
+  
+  removeBookFromList = (listName, book) => {
+    const books = {...this.state.bookLists[listName].books};
+    books[book] = null;
+    this.setState(newState => {
+      return {
+        bookLists: update(this.state.bookLists, {[listName]: {books: {$set: books}}})
+      };
     });
   };
   
@@ -107,7 +128,7 @@ class App extends React.Component {
     this.setState(newState => {
       return {
         bookLists: update(newState.bookLists, {[listName]: {selected: {$set: selected}}})
-      }
+      };
     });
   };
     
