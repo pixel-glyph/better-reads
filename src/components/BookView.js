1f5122c9009f0e179b9055101760a654c451da81
andrewjensen <andrew.jensen-battaglia@jorsek.com>
2017-07-24 21:28:51 -0400

    add book view elements and basic styling

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -12,8 +12,10 @@ class BookView extends React.Component {
     const bookInfo = this.props.location.bookInfo || this.props.bookInfo;
     return (
       <div className="book-view-wrapper">
-        <img src={bookInfo.img} alt="book cover"/>
-        <div>{this.props.bookID}</div>
+        <img src={bookInfo.img} alt="book cover" className="book-view-cover"/>
+        <div className="book-view-title">{bookInfo.title}</div>
+        <div className="book-view-author">{bookInfo.author}</div>
+        <p className="book-view-desc">{bookInfo.desc}</p>
       </div>
     )
   }
@@ -22,7 +24,10 @@ class BookView extends React.Component {
 BookView.propTypes = {
   bookID: PropTypes.string.isRequired,
   setBookView: PropTypes.func.isRequired,
-  // bookInfo: PropTypes.object
+  bookInfo: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.bool
+  ])
 };
 
 export default BookView;
