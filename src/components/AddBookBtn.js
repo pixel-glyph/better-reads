372ebb3681255de51223e980c08ccbbb2bb44b52
andrewjensen <andrew.jensen-battaglia@jorsek.com>
2017-07-30 22:24:23 -0400

    modify button markup, add test click method

--- src/components/AddBookBtn.js
+++ src/components/AddBookBtn.js
@@ -2,19 +2,28 @@ import React from 'react';
 import PlusIcon from './svg/Plus';
 import PropTypes from 'prop-types';
 
-const AddBookBtn = () => {
-  return (
-    <div className="book-view-btns">
-      <div className="add-book-btn-wrapper">  
-        <div 
-          className="add-book-btn on-search" 
-          onClick={() => this.props.addNewBook('To Read', this.props.bookInfo)}>
-          <span>To Read</span>
+class AddBookBtn extends React.Component {
+  
+  showAddMenu = () => {
+    return console.log('showing!');
+  };
+  
+  render() {
+    return (
+      <div className="book-view-btns">
+        <div className="add-book-btn-wrapper">  
+          <div 
+            className="add-book-btn on-search" 
+            onClick={() => this.props.addNewBook('To Read', this.props.bookInfo)}>
+            <span>To Read</span>
+          </div>
+          <div className="btn-icon-wrapper" onClick={() => this.showAddMenu()}>
+            <PlusIcon/>
+          </div>
         </div>
-        <PlusIcon/>
       </div>
-    </div>
-  )
+    )
+  }
 };
 
 AddBookBtn.propTypes = {
