71cccbc490d8ef4a19e84661d0656c45158b7494
andrewjensen <andrew.jensen-battaglia@jorsek.com>
2017-08-15 09:18:16 -0400

    styling for main list page

--- src/components/BookListPane.js
+++ src/components/BookListPane.js
@@ -26,7 +26,7 @@ class BookListPane extends React.Component {
   render() {
     return (
       <div className="app-book-list-pane">
-        <h1 className="list-title">List Title</h1>
+        <h1 className="list-title">{this.props.currentList.listName}</h1>
         <ul className="app-book-list">
           {this.renderList()}
         </ul>
